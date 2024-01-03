fe656292784e1b1ae6a02831a8a7402d4a04d50f
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2024-01-03 18:30:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "madhu" {
   ami                    = "ami-01216e7612243e0ef"
   instance_type          = "t2.micro"
   key_name               = "mumbai"
-    user_data              = file("install.sh")
+    user_data              = file("website.sh")
   tags = {
     Name = "vcube"
   }
