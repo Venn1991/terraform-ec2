d450e3b47efe1387fb458e561bf32684b502d67f
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2024-01-03 13:50:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "madhu" {
   ami                    = "ami-01216e7612243e0ef"
   instance_type          = "t2.micro"
   key_name               = "mumbai"
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  user_data              = file("website.sh")
+    user_data              = file("website.sh")
   tags = {
     Name = "vcube"
   }
